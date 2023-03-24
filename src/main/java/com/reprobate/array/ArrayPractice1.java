dd0c6238f1c110065aa8f0046650c1073ca4b573
Reprobate <924427411@qq.com>
2023-03-24 09:20:38 +0800

    题解1

--- src/main/java/com/reprobate/array/ArrayPractice1.java
+++ src/main/java/com/reprobate/array/ArrayPractice1.java
@@ -25,21 +25,32 @@ public class ArrayPractice1 {
      * 如果给定的数组已经排好序呢？将如何优化你的算法呢？
      */
     public static void main(String[] args) {
-
+        System.out.println(Arrays.toString(mySolution(new int[]{1, 2, 2, 1}, new int[]{2, 2})));
     }
 
     private static int[] mySolution(int[] nums1, int[] nums2) {
-        Set<Integer> set = new HashSet<>();
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i : nums1) {
-            set.add(i);
+            Integer num = map.get(i);
+            map.put(i, num == null ? 1 : num + 1);
         }
-        Set<Integer> resultSet = new HashSet<>();
+        List<Integer> list = new ArrayList<>(Math.max(nums1.length, nums2.length));
         for (int i : nums2) {
-            if (set.contains(i)) {
-                resultSet.add(i);
+            Integer num = map.get(i);
+            if (num != null) {
+                list.add(i);
+                if (num - 1 == 0) {
+                    map.remove(i);
+                } else {
+                    map.put(i, num - 1);
+                }
             }
         }
-        return null;
+        int[] result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+        return result;
     }
 
     private static int[] mySolutionAdvance(int[] nums1, int[] nums2) {
@@ -47,6 +58,23 @@ public class ArrayPractice1 {
     }
 
     private static int[] solution(int[] nums1, int[] nums2) {
-        return null;
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int length1 = nums1.length, length2 = nums2.length;
+        int[] intersection = new int[Math.min(length1, length2)];
+        int index1 = 0, index2 = 0, index = 0;
+        while (index1 < length1 && index2 < length2) {
+            if (nums1[index1] < nums2[index2]) {
+                index1++;
+            } else if (nums1[index1] > nums2[index2]) {
+                index2++;
+            } else {
+                intersection[index] = nums1[index1];
+                index1++;
+                index2++;
+                index++;
+            }
+        }
+        return Arrays.copyOfRange(intersection, 0, index);
     }
 }
